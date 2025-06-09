c9181744450f1e117acb8624a40147070ac64ed9
Jose Rosales <167634761+Polaryys@users.noreply.github.com>
2025-06-08 23:39:28 -0600

    Cambios a interfaces de funciones varias

--- src/EstructuraDatos.java
+++ src/EstructuraDatos.java
@@ -14,6 +14,29 @@ public class EstructuraDatos extends JFrame {
         setLocationRelativeTo(null);
 
         setLayout(null);
+        
+        JLabel label1 = new JLabel("Últimas acciones");
+        label1.setFont(new Font("Arial", Font.PLAIN, 24));
+        label1.setBounds(100, 80, 600, 40); // Posición y tamaño
+
+        JLabel label2 = new JLabel();
+        label2.setFont(new Font("Arial", Font.PLAIN, 24));
+        label2.setBounds(100, 130, 300, 40);
+        label2.setBackground(Color.LIGHT_GRAY);
+        label2.setOpaque(true);
+
+        JLabel label3 = new JLabel();
+        label3.setFont(new Font("Arial", Font.PLAIN, 24));
+        label3.setBounds(100, 180, 300, 40);
+        label3.setBackground(Color.LIGHT_GRAY);
+        label3.setOpaque(true);
+
+        JLabel label4 = new JLabel();
+        label4.setFont(new Font("Arial", Font.PLAIN, 24));
+        label4.setBounds(100, 230, 300, 40);
+        label4.setBackground(Color.LIGHT_GRAY);
+        label4.setOpaque(true);
+        
         JButton button1 = new JButton("Atrás");
         button1.setFont(new Font("Arial", Font.PLAIN, 18));
         button1.setBounds(20, 30, 100, 30); // Posición y tamaño
@@ -29,7 +52,10 @@ public class EstructuraDatos extends JFrame {
 
 
         add(button1);
-     
+        add(label1);
+        add(label2);
+        add(label3);
+        add(label4);
     }
     public static void main(String[] args) {
     SwingUtilities.invokeLater(() -> {
